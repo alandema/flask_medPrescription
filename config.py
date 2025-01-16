3bffb932c0d8f8d549fbf224f37ce75ce0bc9af7
Alan <alann.maman@gmail.com>
2025-01-16 18:35:55 -0300

    new config schema

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB')
