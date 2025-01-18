51211a1ed7fcaa54e7b413685fe7b4ec24be1ef9
alandema <alann.maman@gmail.com>
2025-01-18 18:10:28 -0300

    using model

--- database.py
+++ database.py
@@ -4,6 +4,13 @@ from config import Config
 db = SQLAlchemy()
 
 
+class Users(db.Model):
+    __tablename__ = 'users'  # Specify the existing table name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+
 def init_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{Config.MYSQL_USER}:{Config.MYSQL_PASSWORD}@{Config.MYSQL_HOST}/{Config.MYSQL_DB}"
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 280}
--- routes.py
+++ routes.py
@@ -1,9 +1,8 @@
 from flask import render_template, current_app
-from flask import render_template
-from database import db, User
+from database import db, Users
 
 
 @current_app.route('/')
 def index():
-    rows = db.session.query(User).limit(5).all()
+    rows = db.session.query(Users).limit(5).all()
     return render_template('index.html', rows=rows)
