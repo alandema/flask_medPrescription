c7253612443f42ff57b3d271db4b3498d397c930
alandema <alann.maman@gmail.com>
2025-01-18 17:58:44 -0300

    sql alchemy

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
-import mysql.connector
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-def get_db_connection():
-    return mysql.connector.connect(
-        host=Config.MYSQL_HOST,
-        user=Config.MYSQL_USER,
-        password=Config.MYSQL_PASSWORD,
-        database=Config.MYSQL_DB
-    )
+db = SQLAlchemy()
+
+
+def init_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{Config.MYSQL_USER}:{Config.MYSQL_PASSWORD}@{Config.MYSQL_HOST}/{Config.MYSQL_DB}"
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 280}
+    db.init_app(app)
--- flask_app.py
+++ flask_app.py
@@ -3,11 +3,13 @@ load_dotenv('config.env')  # noqa
 
 from config import Config
 from flask import Flask
+from database import init_db
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    init_db(app)  # Add this line
 
     # Import routes from main and chatbot logic
     with app.app_context():
