2ac5e73f61ac08d4159d66e3b9225d90a12fede5
alandema <alann.maman@gmail.com>
2025-01-18 18:03:27 -0300

    modify route

--- routes.py
+++ routes.py
@@ -1,13 +1,9 @@
 from flask import render_template, current_app
-from database import get_db_connection
+from flask import render_template
+from database import db, User
 
 
 @current_app.route('/')
 def index():
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT id, name, age FROM users LIMIT 5")
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    rows = db.session.query(User).limit(5).all()
     return render_template('index.html', rows=rows)
