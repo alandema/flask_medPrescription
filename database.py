81232cb66c5a857b4353b02a54a05dccf44e212b
alandema <alann.maman@gmail.com>
2025-01-18 18:17:16 -0300

    fix URI

--- database.py
+++ database.py
@@ -12,6 +12,6 @@ class Users(db.Model):
 
 
 def init_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{Config.MYSQL_USER}:{Config.MYSQL_PASSWORD}@{Config.MYSQL_HOST}/{Config.MYSQL_DB}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqldb://{Config.MYSQL_USER}:{Config.MYSQL_PASSWORD}@{Config.MYSQL_HOST}/{Config.MYSQL_DB}"
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 280}
     db.init_app(app)
